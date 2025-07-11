ac74ba889b35885e4c63f3fffc4735d1fe767d50
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2025-07-11 16:30:05 +0000

    fix inject

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,9 +1,9 @@
 // Author: Musicminion
 // Description: Adds a custom avatar,msvalidate to the top of the page
-hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="./css/my-avatar.css">', 'default');
+hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="./css/my-avatar.css">', 'home');
 
 // Description: Adds banner transition effect
-hexo.extend.injector.register('head_begin', '<link rel="stylesheet" href="./css/my-banner.css">', 'default');
+hexo.extend.injector.register('head_begin', '<link rel="stylesheet" href="./css/my-banner.css">', 'home');
 
 // Add msvalidate
 hexo.extend.injector.register('head_begin', '<meta name="msvalidate.01" content="3A0A333C87F53DC35E793673A0667DFC" />', 'default');
